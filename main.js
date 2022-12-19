9986daff206f8d934b59ba1712b455c119578ebc
KrishivAgarwal29 <86786425+KrishivAgarwal29@users.noreply.github.com>
2022-12-19 18:52:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+video="";
+status=""
+objects=[]
+
+function preload(){
+    video = createVideo("video.mp4")
+    video.hide()
+}
+
+function setup(){
+    canvas = createCanvas(480, 300);
+    canvas.center()
+}
+
+function draw(){
+   image(video, 0, 0, 480, 300) 
+   if(status !=""){
+    objectDetector.detect(video, gotResult)
+    for(i=0; i<objects.length; i++){
+        document.getElementById("status").innerHTML = "Status: Objects Detected";
+        document.getElementById("number_of_objects").innerHTML = "Number of Objects Detetected :" + objects.length
+        fill("red");
+        percent = floor(objects[i].confidence * 100)
+        text(objects[i].label + "" + percent + "%", objects[i].x+15, objects[i].y+15)
+        noFill()
+        stroke("red")
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)  
+    }}}
+
+function modelLoaded(){
+    status=true;
+    console.log("Model Loade!")
+    video.loop();
+    video.speed(1);
+    video.volume(1);
+    
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('Cosossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+
+}
